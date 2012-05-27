2dc734152485a3acb8759adf27de6837d1b3ca3d
Alexander Schlueter <arleckshunt@googlemail.com>
2012-05-27 20:23:49 +0200

    Basic class structure, working append, prepend, clear, copy. Using nodewise COW.

--- List.cpp
+++ List.cpp
@@ -1,1 +1,79 @@
-#include "List.h"
+//#include "List.h"
+
+template <class T>
+List<T>::List(const List<T> &other) : size(other.getSize())
+{
+    if (getSize() != 0) {
+        first = new Node(*(other.first)),
+        last = first;
+        for (Node *other_node = other.first->next; other_node != NULL; other_node = other_node->next) {
+            last->next = new Node(*other_node, last);
+            last = last->next;
+        }
+    }
+}
+
+template <class T>
+void List<T>::append(const T &item)
+{
+    if (last == NULL) {
+        last = new Node(item);
+        first = last;
+    } else {
+        last->next = new Node(item, last);
+        last = last->next;
+        size++;
+    }
+}
+
+template <class T>
+void List<T>::prepend(const T &item)
+{
+    if (first == NULL)
+    {
+        first = new Node(item);
+        last = first;
+    } else {
+        first->previous = new Node(item, NULL, first);
+        first = first->previous;
+        size++;
+    }
+}
+
+template <class T>
+T List<T>::at(unsigned int pos) const
+{
+    if (pos > size - 1) throw 0; //out_of_range
+    else {
+        List<T>::iterator it = this->begin();
+        for (int i = 0; i < pos; i++)
+            ++it;
+        return *it;
+    }
+}
+
+template <class T>
+typename List<T>::Node::NodeData *List<T>::Node::NodeData::copy()
+{
+    refcount++;
+    return this;
+}
+
+template <class T>
+void List<T>::Node::NodeData::release()
+{
+    if (--refcount = 0) delete this;
+}
+
+template <class T>
+void List<T>::clear()
+{
+    if (getSize() != 0) {
+        for (Node *current_node = first->next; current_node != NULL; current_node = current_node->next)
+            delete current_node->previous;
+        delete last;
+    }
+    first = NULL;
+    last = NULL;
+    size = 0;
+}
--- List.h
+++ List.h
@@ -1,12 +1,63 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <cstdlib> //not here
+
+template <class T>
 class List
 {
 private:
+    struct Node
+    {
+        struct NodeData
+        {
+            T item;
+            unsigned int refcount;
+
+            NodeData(const T &item) : item(item), refcount(1) {};
+
+            NodeData *copy();
+            void release();
+        };
+
+        NodeData *data;
+        Node *previous;
+        Node *next;
+
+        Node(const Node &other, Node *previous = NULL, Node *next = NULL) : data(other.data->copy()), previous(previous), next(next) {};
+        Node(const T &item, Node *previous = NULL, Node *next = NULL) : data(new NodeData(item)), previous(previous), next(next) {};
+        ~Node() { data->release(); };
+    };
+
+    class iterator
+    {
+    private:
+        Node *item;
+
+    public:
+
+    };
+
+    Node *first;
+    Node *last;
+    unsigned int size;
 
 public:
+    List() : first(NULL), last(NULL), size(0) {};
+    List(const List<T> &other);
 
+    void append(const T &item);
+    void prepend(const T &item);
+    void insert(const T &item, unsigned int before_pos);
+    void remove(unsigned int pos);
+    void clear();
+
+    iterator begin() const;
+    iterator end() const;
+    T at(unsigned int pos) const;
+    inline unsigned int getSize() const { return this->size; };
 };
+
+#include "List.cpp"
 
 #endif //LIST_H
