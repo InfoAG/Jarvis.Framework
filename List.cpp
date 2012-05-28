857b36f4201ff0a6b1aa749392c7a4cd9ca9a5ef
Alexander Schlueter <arleckshunt@googlemail.com>
2012-05-28 16:21:21 +0200

    access items, doxygen comments

--- List.cpp
+++ List.cpp
@@ -16,7 +16,7 @@ List<T>::List(const List<T> &other) : size(other.getSize())
 template <class T>
 void List<T>::append(const T &item)
 {
-    if (last == NULL) {
+    if (last == 0) {
         last = new Node(item);
         first = last;
     } else {
@@ -29,19 +29,19 @@ void List<T>::append(const T &item)
 template <class T>
 void List<T>::prepend(const T &item)
 {
-    if (first == NULL)
+    if (first == 0)
     {
         first = new Node(item);
         last = first;
     } else {
-        first->previous = new Node(item, NULL, first);
+        first->previous = new Node(item, 0, first);
         first = first->previous;
     }
     size++;
 }
 
 template <class T>
-T List<T>::at(unsigned int pos) const
+const T &List<T>::at(unsigned int pos) const
 {
     if (pos > size - 1) throw 0; //out_of_range
     else {
@@ -53,28 +53,15 @@ T List<T>::at(unsigned int pos) const
 }
 
 template <class T>
-typename List<T>::Node::NodeData *List<T>::Node::NodeData::copy()
-{
-    refcount++;
-    return this;
-}
-
-template <class T>
-void List<T>::Node::NodeData::release()
-{
-    if (--refcount = 0) delete this;
-}
-
-template <class T>
 void List<T>::clear()
 {
     if (getSize() != 0) {
-        for (Node *current_node = first->next; current_node != NULL; current_node = current_node->next)
+        for (Node *current_node = first->next; current_node != 0; current_node = current_node->next)
             delete current_node->previous;
         delete last;
     }
-    first = NULL;
-    last = NULL;
+    first = 0;
+    last = 0;
     size = 0;
 }
 
@@ -102,11 +89,22 @@ void List<T>::remove(unsigned int pos)
         Node *target_node = first;
         for (unsigned int i = 0; i < pos; i++)
             target_node = target_node->next;
-        if (target_node->previous != NULL) target_node->previous->next = target_node->next;
+        if (target_node->previous != 0) target_node->previous->next = target_node->next;
         else first = target_node->next;
-        if (target_node->next != NULL) target_node->next->previous = target_node->previous;
+        if (target_node->next != 0) target_node->next->previous = target_node->previous;
         else last = target_node->previous;
         delete target_node;
         size--;
     }
+}
+
+template <class T>
+T &List<T>::Node::getItem()
+{
+    if (data->refcount > 1) {
+        NodeData *new_data = new NodeData(data->item);
+        data->release();
+        data = new_data;
+    }
+    return data->item;
 }
--- List.h
+++ List.h
@@ -1,60 +1,127 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include <cstdlib> //not here
+#include "Container.h"
+
+/** @brief List class implemented as doubly linked list with nodewise COW.
+  *
+  * Optimized for copying of lists with large elements, insert / remove.
+  * Suboptimal for small elements and random access.
+  *
+  * @author Alexander Schlüter
+  */
 
 template <class T>
-class List
+class List : Container
 {
 private:
+    //! A list node.
     struct Node
     {
+        //! Smart node data for COW.
         struct NodeData
         {
-            T item;
-            unsigned int refcount;
+            T item; /**< Actual item data. */
+            unsigned int refcount; /**< Number of nodes referencing this data. */
 
+            /** Constructor.
+              * @param item as the item to encapsulate.
+              */
             NodeData(const T &item) : item(item), refcount(1) {};
-
-            NodeData *copy();
-            void release();
+            /** Adds a reference for a new node.
+              * @return A pointer to this.
+              * @see release()
+              */
+            NodeData *copy() { refcount++; return this; };
+            /** Removes a reference and deletes when appropriate.
+              * @see copy()
+              */
+            void release() { if (--refcount == 0) delete this; };
         };
 
-        NodeData *data;
-        Node *previous;
-        Node *next;
+        NodeData *data; /**< Pointer to the NodeData object containing item data.*/
+        Node *previous; /**< Pointer to previous node in list. */
+        Node *next; /**< Pointer to next node in list. */
 
-        Node(const Node &other, Node *previous = NULL, Node *next = NULL) : data(other.data->copy()), previous(previous), next(next) {};
-        Node(const T &item, Node *previous = NULL, Node *next = NULL) : data(new NodeData(item)), previous(previous), next(next) {};
+        /** Copy constructor.
+          * @param other Node to copy.
+          * @param previous Pointer to previous node.
+          * @param next Pointer to next node.
+          */
+        Node(const Node &other, Node *previous = 0, Node *next = 0) : data(other.data->copy()), previous(previous), next(next) {};
+        /** Copy constructor.
+          * @param item Item to store.
+          * @param previous Pointer to previous node.
+          * @param next Pointer to next node.
+          */
+        Node(const T &item, Node *previous = 0, Node *next = 0) : data(new NodeData(item)), previous(previous), next(next) {};
+        /** Destructor.
+          * @see release()
+          */
         ~Node() { data->release(); };
+
+        /**
+          * @return Returns a reference to the item.
+          */
+        inline T &getItem();
     };
 
+    Node *first; /**< Pointer to first node in list.*/
+    Node *last; /**< Pointer to last node in list.*/
+    unsigned int size; /**< List size.*/
+
+public:
     class iterator
     {
     private:
-        Node *item;
+        Node *target; /**< Pointer to targeted node.*/
 
     public:
+        /** Constructor.
+          * @param target Pointer to target node.
+          */
+        iterator(Node *target = 0) : target(target) {};
 
+        T &operator*() const { return target->getItem(); };
+        iterator &operator++() { target = target->next; return *this; };
+        iterator &operator++(int) { target = target->next; return iterator(target->previous); };
+        bool operator==(const iterator &other) const { return target == other.target; };
+        bool operator!=(const iterator &other) const { return target != other.target; };
     };
 
-    Node *first;
-    Node *last;
-    unsigned int size;
-
-public:
-    List() : first(NULL), last(NULL), size(0) {};
+    List() : first(0), last(0), size(0) {};
     List(const List<T> &other);
 
+    /** Append one item to list.
+      * @param item The item to append.
+      */
     void append(const T &item);
+    /** Prepend one item to list.
+      * @param item The item to prepend.
+      */
     void prepend(const T &item);
+    /** Insert one item into list.
+      * @param item The item to insert.
+      * @param pos The position for the new item.
+      */
     void insert(const T &item, unsigned int pos);
+    /** Remove one item from list.
+      * @param pos Position of the item to remove.
+      */
     void remove(unsigned int pos);
+    /** Delete all list items. */
     void clear();
 
-    iterator begin() const;
-    iterator end() const;
-    T at(unsigned int pos) const;
+    /** @return Returns an iterator to the first item of the list.  */
+    iterator begin() const { return iterator(first); };
+    /** @warning broken @return Returns an iterator that sucks.  */
+    iterator end() const { return iterator(); };
+    /** Access one element based on its position.
+      * @param pos The position to access.
+      * @return A reference to the item.
+      */
+    const T &at(unsigned int pos) const;
+    /** @return The list's size.*/
     inline unsigned int getSize() const { return this->size; };
 };
 
