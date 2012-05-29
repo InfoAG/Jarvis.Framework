8f9f11758e8ba7c8f5501e3310a93c2add81f3fa
Alexander Schlueter <arleckshunt@googlemail.com>
2012-05-29 14:59:43 +0200

    Added operators, iterators, replaced NULL

--- List.cpp
+++ List.cpp
@@ -1,12 +1,12 @@
 //#include "List.h"
 
 template <class T>
-List<T>::List(const List<T> &other) : size(other.getSize())
+List<T>::List(const List<T> &other) : _size(other.size())
 {
-    if (getSize() != 0) {
+    if (! isEmpty()) {
         first = new Node(*(other.first)),
         last = first;
-        for (Node *other_node = other.first->next; other_node != NULL; other_node = other_node->next) {
+        for (Node *other_node = other.first->next; other_node != 0; other_node = other_node->next) {
             last->next = new Node(*other_node, last);
             last = last->next;
         }
@@ -23,7 +23,7 @@ void List<T>::append(const T &item)
         last->next = new Node(item, last);
         last = last->next;
     }
-    size++;
+    _size++;
 }
 
 template <class T>
@@ -37,54 +37,49 @@ void List<T>::prepend(const T &item)
         first->previous = new Node(item, 0, first);
         first = first->previous;
     }
-    size++;
+    _size++;
 }
 
 template <class T>
 const T &List<T>::at(unsigned int pos) const
 {
-    if (pos > size - 1) throw 0; //out_of_range
-    else {
-        List<T>::iterator it = this->begin();
-        for (int i = 0; i < pos; i++)
-            ++it;
-        return *it;
-    }
+    if (pos > size() - 1) throw 0; //out_of_range
+    else return (*this)[pos];
 }
 
 template <class T>
 void List<T>::clear()
 {
-    if (getSize() != 0) {
+    if (! isEmpty()) {
         for (Node *current_node = first->next; current_node != 0; current_node = current_node->next)
             delete current_node->previous;
         delete last;
     }
     first = 0;
     last = 0;
-    size = 0;
+    _size = 0;
 }
 
 template <class T>
 void List<T>::insert(const T &item, unsigned int pos)
 {
     if (pos == 0) prepend(item);
-    else if (pos == getSize()) append(item);
-    else if (pos > getSize()) throw 0; //out_of_range
+    else if (pos == size()) append(item);
+    else if (pos > size()) throw 0; //out_of_range
     else {
         Node *previous_node = first;
         for (unsigned int i = 0; i < pos - 1; i++)
             previous_node = previous_node->next;
         previous_node->next->previous = new Node(item, previous_node, previous_node->next);
         previous_node->next = previous_node->next->previous;
-        size++;
+        _size++;
     }
 }
 
 template <class T>
 void List<T>::remove(unsigned int pos)
 {
-    if (pos >= getSize()) throw 0; //out_of_range
+    if (pos >= size()) throw 0; //out_of_range
     else {
         Node *target_node = first;
         for (unsigned int i = 0; i < pos; i++)
@@ -94,8 +89,16 @@ void List<T>::remove(unsigned int pos)
         if (target_node->next != 0) target_node->next->previous = target_node->previous;
         else last = target_node->previous;
         delete target_node;
-        size--;
+        _size--;
     }
+}
+
+template <class T>
+inline T &List<T>::operator[](unsigned int pos) const
+{
+    iterator it = this->begin();
+    while (pos--) ++it;
+    return *it;
 }
 
 template <class T>
@@ -107,4 +110,62 @@ T &List<T>::Node::getItem()
         data = new_data;
     }
     return data->item;
+}
+
+template <class T>
+typename List<T>::BaseIterator &List<T>::BaseIterator::operator--()
+{
+    if (target == 0) target = baseList->last;
+    else target = target->previous;
+    return *this;
+}
+
+template <class T>
+typename List<T>::BaseIterator List<T>::BaseIterator::operator--(int)
+{
+    if (target == 0) target = baseList->last;
+    else target = target->previous;
+    return BaseIterator(target->next, baseList);
+}
+
+template <class T>
+typename List<T>::BaseIterator List<T>::BaseIterator::operator+(int n) const
+{
+    BaseIterator result(*this);
+    if (n > 0)
+        while (n--) ++result;
+    else
+        while (n++) --result;
+    return result;
+}
+
+template <class T>
+bool List<T>::operator==(const List &other) const
+{
+    if (size() != other.size()) return false;
+    else {
+        for (const_BaseIterator it = begin(), it_other = other.begin(); it != end(); ++it, ++it_other)
+            if (*it != *it_other) return false;
+        return true;
+    }
+}
+
+template <class T>
+List<T> &List<T>::operator +=(const List &other)
+{
+    if (! other.isEmpty()) {
+        if (isEmpty()) {
+            last = new Node(*(other.first));
+            first = last;
+        } else {
+            last->next = new Node(*(other.first));
+            last = last->next;
+        }
+        for (Node *other_node = other.first->next; other_node != 0; other_node = other_node->next) {
+            last->next = new Node(*other_node, last);
+            last = last->next;
+        }
+        _size += other.size();
+    }
+    return *this;
 }
--- List.h
+++ List.h
@@ -27,16 +27,16 @@ private:
             /** Constructor.
               * @param item as the item to encapsulate.
               */
-            NodeData(const T &item) : item(item), refcount(1) {};
+            inline NodeData(const T &item) : item(item), refcount(1) {};
             /** Adds a reference for a new node.
               * @return A pointer to this.
               * @see release()
               */
-            NodeData *copy() { refcount++; return this; };
+            inline NodeData *copy() { refcount++; return this; };
             /** Removes a reference and deletes when appropriate.
               * @see copy()
               */
-            void release() { if (--refcount == 0) delete this; };
+            inline void release() { if (--refcount == 0) delete this; };
         };
 
         NodeData *data; /**< Pointer to the NodeData object containing item data.*/
@@ -48,17 +48,17 @@ private:
           * @param previous Pointer to previous node.
           * @param next Pointer to next node.
           */
-        Node(const Node &other, Node *previous = 0, Node *next = 0) : data(other.data->copy()), previous(previous), next(next) {};
+        inline Node(const Node &other, Node *previous = 0, Node *next = 0) : data(other.data->copy()), previous(previous), next(next) {};
         /** Copy constructor.
           * @param item Item to store.
           * @param previous Pointer to previous node.
           * @param next Pointer to next node.
           */
-        Node(const T &item, Node *previous = 0, Node *next = 0) : data(new NodeData(item)), previous(previous), next(next) {};
+        inline Node(const T &item, Node *previous = 0, Node *next = 0) : data(new NodeData(item)), previous(previous), next(next) {};
         /** Destructor.
           * @see release()
           */
-        ~Node() { data->release(); };
+        inline ~Node() { data->release(); };
 
         /**
           * @return Returns a reference to the item.
@@ -66,30 +66,68 @@ private:
         inline T &getItem();
     };
 
-    Node *first; /**< Pointer to first node in list.*/
-    Node *last; /**< Pointer to last node in list.*/
-    unsigned int size; /**< List size.*/
-
-public:
-    class iterator
+    //! Abstract base class for list iterators.
+    class BaseIterator
     {
     private:
+        const List *baseList; /**< Pointer to the list this iterator is based on. */
+
+    protected:
         Node *target; /**< Pointer to targeted node.*/
 
     public:
         /** Constructor.
           * @param target Pointer to target node.
+          * @param baseList Pointer to base list.
           */
-        iterator(Node *target = 0) : target(target) {};
+        inline BaseIterator(Node *target = 0, const List  *baseList = 0) : target(target), baseList(baseList) {};
+        /** Copy constructor.
+          * @param other Reference of the list to copy.
+          */
+        inline BaseIterator(const BaseIterator &other) : target(other.target), baseList(other.baseList) {};
 
-        T &operator*() const { return target->getItem(); };
-        iterator &operator++() { target = target->next; return *this; };
-        iterator &operator++(int) { target = target->next; return iterator(target->previous); };
-        bool operator==(const iterator &other) const { return target == other.target; };
-        bool operator!=(const iterator &other) const { return target != other.target; };
+        inline BaseIterator &operator++() { target = target->next; return *this; };
+        inline BaseIterator operator++(int) { target = target->next; return BaseIterator(target->previous, baseList); };
+        inline BaseIterator &operator--();
+        inline BaseIterator operator--(int);
+        inline bool operator==(const BaseIterator &other) const { return (target == other.target && baseList == other.baseList); };
+        inline bool operator!=(const BaseIterator &other) const { return ! (*this == other); };
+        inline BaseIterator operator+(int n) const;
+        inline BaseIterator operator-(int n) const { return operator+(-n); };
+        inline BaseIterator &operator+=(int n) { return *this = *this + n; };
+        inline BaseIterator &operator-=(int n) { return *this = *this - n; };
+
+        const Node * const getNode() const { return target; };
+        const List * const getBaseList() const { return baseList; };
     };
 
-    List() : first(0), last(0), size(0) {};
+    Node *first; /**< Pointer to first node in list.*/
+    Node *last; /**< Pointer to last node in list.*/
+    unsigned int _size; /**< List size.*/
+
+public:
+    //! List iterator.
+    class iterator : public BaseIterator
+    {
+    public:
+        inline iterator(Node *target = 0, const List *baseList = 0) : BaseIterator(target, baseList) {};
+        inline iterator(const BaseIterator &other) : BaseIterator(other) {};
+
+        inline T &operator*() { return target->getItem(); };
+    };
+
+    //! Const list iterator.
+    class const_iterator : public BaseIterator
+    {
+    public:
+        inline const_iterator(Node *target = 0, const List *baseList = 0) : BaseIterator(target, baseList) {};
+        inline const_iterator(const BaseIterator &other) : BaseIterator(other) {};
+
+        inline const T &operator*() { return target->getItem(); };
+
+    };
+
+    inline List() : first(0), last(0), _size(0) {};
     List(const List<T> &other);
 
     /** Append one item to list.
@@ -113,16 +151,30 @@ public:
     void clear();
 
     /** @return Returns an iterator to the first item of the list.  */
-    iterator begin() const { return iterator(first); };
-    /** @warning broken @return Returns an iterator that sucks.  */
-    iterator end() const { return iterator(); };
+    inline iterator begin() { return iterator(first, this); };
+    /** @return Returns a const_iterator to the first item of the list.  */
+    inline const_iterator begin() const { return const_iterator(first, this); };
+    /** @return Returns an iterator pointing after the last item in the list.  */
+    inline iterator end() { return iterator(0, this); };
+    /** @return Returns a const_iterator pointing after the last item in the list.  */
+    inline const_iterator end() const { return const_iterator(0, this); };
     /** Access one element based on its position.
       * @param pos The position to access.
       * @return A reference to the item.
       */
     const T &at(unsigned int pos) const;
+    /** @return Returns true if the list is empty. */
+    inline bool isEmpty() const { return size == 0; };
     /** @return The list's size.*/
-    inline unsigned int getSize() const { return this->size; };
+    inline unsigned int size() const { return _size; };
+
+    bool operator==(const List &other) const;
+    inline bool operator!=(const List &other) const { return (! *this == other); };
+    inline List operator+(const T &item) const { List result(*this); result.append(item); return result; };
+    inline List operator+(const List &other) const { List result(*this); result += other; return result; };
+    inline List &operator+=(const T &item) { append(item); return *this; }
+    inline List &operator+=(const List &other);
+    inline T &operator[](unsigned int pos) const;
 };
 
 #include "List.cpp"
