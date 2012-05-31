5dc870970d407934bf06dfe05bd7ac62683b4e34
Alexander Schlueter <arleckshunt@googlemail.com>
2012-05-31 22:03:18 +0200

    implemented SmartList, RepetitiveList

--- List.cpp
+++ List.cpp
@@ -1,7 +1,136 @@
-//#include "List.h"
+template <class T, class _Node>
+List<T, _Node>::List(const List<T, _Node> &other) : _size(other.size())
+{
+    if (! other.isEmpty()) {
+        first = other.first->copy(),
+        last = first;
+        for (AbstractNode<T> *other_node = other.first->next; other_node != 0; other_node = other_node->next) {
+            last->next = other_node->copy(last);
+            last = last->next;
+        }
+    }
+}
 
-template <class T>
-List<T>::List(const List<T> &other) : _size(other.size())
+template <class T, class _Node>
+void List<T, _Node>::append(const T &item)
+{
+    if (last == 0) {
+        last = new _Node(item);
+        first = last;
+    } else {
+        last->next = new _Node(item, last);
+        last = last->next;
+    }
+    _size++;
+}
+
+template <class T, class _Node>
+void List<T, _Node>::prepend(const T &item)
+{
+    if (first == 0)
+    {
+        first = new _Node(item);
+        last = first;
+    } else {
+        first->previous = new _Node(item, 0, first);
+        first = first->previous;
+    }
+    _size++;
+}
+
+template <class T, class _Node>
+void List<T, _Node>::insert(const T &item, unsigned int pos)
+{
+    if (pos == 0) prepend(item);
+    else if (pos == size()) append(item);
+    else if (pos > size()) throw 0; //out_of_range
+    else {
+        AbstractNode<T> *previous_node = first;
+        for (unsigned int i = 0; i < pos - 1; i++)
+            previous_node = previous_node->next;
+        previous_node->next->previous = new _Node(item, previous_node, previous_node->next);
+        previous_node->next = previous_node->next->previous;
+        _size++;
+    }
+}
+
+template <class T, class _Node>
+void List<T, _Node>::remove(unsigned int pos)
+{
+    if (pos >= size()) throw 0; //out_of_range
+    else {
+        AbstractNode<T> *target_node = first;
+        for (unsigned int i = 0; i < pos; i++)
+            target_node = target_node->next;
+        if (target_node->previous != 0) target_node->previous->next = target_node->next;
+        else first = target_node->next;
+        if (target_node->next != 0) target_node->next->previous = target_node->previous;
+        else last = target_node->previous;
+        delete target_node;
+        _size--;
+    }
+}
+
+template <class T, class _Node>
+void List<T, _Node>::clear()
+{
+    if (! isEmpty()) {
+        for (AbstractNode<T> *current_node = first->next; current_node != 0; current_node = current_node->next)
+            delete current_node->previous;
+        delete last;
+    }
+    first = 0;
+    last = 0;
+    _size = 0;
+}
+
+template <class T, class _Node>
+const T &List<T, _Node>::at(unsigned int pos) const
+{
+    if (pos > size() - 1) throw 0; //out_of_range
+    else {
+        iterator it(this->begin());
+        while (pos--) ++it;
+        return *it;
+    }
+
+}
+
+template <class T, class _Node>
+bool List<T, _Node>::operator==(const List &other) const
+{
+    if (size() != other.size()) return false;
+    else {
+        for (const_iterator it = begin(), it_other = other.begin(); it != end(); ++it, ++it_other)
+            if (*it != *it_other) return false;
+        return true;
+    }
+}
+
+template <class T, class _Node>
+List<T, _Node> &List<T, _Node>::operator +=(const List &other)
+{
+    if (! other.isEmpty()) {
+        if (isEmpty()) {
+            last = other.first->copy();
+            first = last;
+        } else {
+            last->next = other.first->copy();
+            last = last->next;
+        }
+        AbstractNode<T> *other_node = other.first->next;
+        for (int i = other.size(); i > 1; i++) {
+            last->next = other_node->copy(last);
+            last = last->next;
+            other_node = other_node->next;
+        }
+        _size += other.size();
+    }
+    return *this;
+}
+
+/*template <class T>
+RepetitiveList<T, _Node>::RepetitiveList(const RepetitiveList<T, _Node> &other) : _size(other.size())
 {
     if (! isEmpty()) {
         first = new Node(*(other.first)),
@@ -14,95 +143,7 @@ List<T>::List(const List<T> &other) : _size(other.size())
 }
 
 template <class T>
-void List<T>::append(const T &item)
-{
-    if (last == 0) {
-        last = new Node(item);
-        first = last;
-    } else {
-        last->next = new Node(item, last);
-        last = last->next;
-    }
-    _size++;
-}
-
-template <class T>
-void List<T>::prepend(const T &item)
-{
-    if (first == 0)
-    {
-        first = new Node(item);
-        last = first;
-    } else {
-        first->previous = new Node(item, 0, first);
-        first = first->previous;
-    }
-    _size++;
-}
-
-template <class T>
-const T &List<T>::at(unsigned int pos) const
-{
-    if (pos > size() - 1) throw 0; //out_of_range
-    else return (*this)[pos];
-}
-
-template <class T>
-void List<T>::clear()
-{
-    if (! isEmpty()) {
-        for (Node *current_node = first->next; current_node != 0; current_node = current_node->next)
-            delete current_node->previous;
-        delete last;
-    }
-    first = 0;
-    last = 0;
-    _size = 0;
-}
-
-template <class T>
-void List<T>::insert(const T &item, unsigned int pos)
-{
-    if (pos == 0) prepend(item);
-    else if (pos == size()) append(item);
-    else if (pos > size()) throw 0; //out_of_range
-    else {
-        Node *previous_node = first;
-        for (unsigned int i = 0; i < pos - 1; i++)
-            previous_node = previous_node->next;
-        previous_node->next->previous = new Node(item, previous_node, previous_node->next);
-        previous_node->next = previous_node->next->previous;
-        _size++;
-    }
-}
-
-template <class T>
-void List<T>::remove(unsigned int pos)
-{
-    if (pos >= size()) throw 0; //out_of_range
-    else {
-        Node *target_node = first;
-        for (unsigned int i = 0; i < pos; i++)
-            target_node = target_node->next;
-        if (target_node->previous != 0) target_node->previous->next = target_node->next;
-        else first = target_node->next;
-        if (target_node->next != 0) target_node->next->previous = target_node->previous;
-        else last = target_node->previous;
-        delete target_node;
-        _size--;
-    }
-}
-
-template <class T>
-inline T &List<T>::operator[](unsigned int pos) const
-{
-    iterator it = this->begin();
-    while (pos--) ++it;
-    return *it;
-}
-
-template <class T>
-T &List<T>::Node::getItem()
+T &RepetitiveList<T, _Node>::Node::getItem()
 {
     if (data->refcount > 1) {
         NodeData *new_data = new NodeData(data->item);
@@ -111,9 +152,9 @@ T &List<T>::Node::getItem()
     }
     return data->item;
 }
-
+*/
 template <class T>
-typename List<T>::BaseIterator &List<T>::BaseIterator::operator--()
+typename AbstractList<T>::BaseIterator &AbstractList<T>::BaseIterator::operator--()
 {
     if (target == 0) target = baseList->last;
     else target = target->previous;
@@ -121,7 +162,7 @@ typename List<T>::BaseIterator &List<T>::BaseIterator::operator--()
 }
 
 template <class T>
-typename List<T>::BaseIterator List<T>::BaseIterator::operator--(int)
+typename AbstractList<T>::BaseIterator AbstractList<T>::BaseIterator::operator--(int)
 {
     if (target == 0) target = baseList->last;
     else target = target->previous;
@@ -129,7 +170,7 @@ typename List<T>::BaseIterator List<T>::BaseIterator::operator--(int)
 }
 
 template <class T>
-typename List<T>::BaseIterator List<T>::BaseIterator::operator+(int n) const
+typename AbstractList<T>::BaseIterator AbstractList<T>::BaseIterator::operator+(int n) const
 {
     BaseIterator result(*this);
     if (n > 0)
@@ -140,32 +181,30 @@ typename List<T>::BaseIterator List<T>::BaseIterator::operator+(int n) const
 }
 
 template <class T>
-bool List<T>::operator==(const List &other) const
+SmartListData<T> *SmartListData<T>::detach()
 {
-    if (size() != other.size()) return false;
+    if (refcount == 1) return this;
     else {
-        for (const_BaseIterator it = begin(), it_other = other.begin(); it != end(); ++it, ++it_other)
-            if (*it != *it_other) return false;
-        return true;
+        refcount--;
+        return new SmartListData(*this);
     }
 }
 
 template <class T>
-List<T> &List<T>::operator +=(const List &other)
+RepetitiveListData<T> *RepetitiveListData<T>::detach()
 {
-    if (! other.isEmpty()) {
-        if (isEmpty()) {
-            last = new Node(*(other.first));
-            first = last;
-        } else {
-            last->next = new Node(*(other.first));
-            last = last->next;
-        }
-        for (Node *other_node = other.first->next; other_node != 0; other_node = other_node->next) {
-            last->next = new Node(*other_node, last);
-            last = last->next;
-        }
-        _size += other.size();
+    if (refcount == 1) return this;
+    else {
+        refcount--;
+        return new RepetitiveListData(*this);
     }
-    return *this;
+}
+
+template <class T>
+typename SmartNode<T>::NodeData *SmartNode<T>::NodeData::detach()
+{
+    if (refcount > 1) {
+        refcount--;
+        return new NodeData(item);
+    } else return this;
 }
--- List.h
+++ List.h
@@ -2,6 +2,7 @@
 #define LIST_H
 
 #include "Container.h"
+#include <iostream>
 
 /** @brief List class implemented as doubly linked list with nodewise COW.
   *
@@ -11,76 +12,58 @@
   * @author Alexander Schlüter
   */
 
+//! A list node.
 template <class T>
-class List : Container
+struct AbstractNode
+{
+    AbstractNode *previous; /**< Pointer to previous node in list. */
+    AbstractNode *next; /**< Pointer to next node in list. */
+
+    /** Copy constructor.
+      * @param other Node to copy.
+      * @param previous Pointer to previous node.
+      * @param next Pointer to next node.
+      */
+    //inline Node(const Node &other, Node *previous = 0, Node *next = 0) : data(other.data->copy()), previous(previous), next(next) {};
+    /** Copy constructor.
+      * @param item Item to store.
+      * @param previous Pointer to previous node.
+      * @param next Pointer to next node.
+      */
+    inline AbstractNode(AbstractNode *previous = 0, AbstractNode *next = 0) : previous(previous), next(next) {};
+    virtual AbstractNode *copy(AbstractNode *previous = 0, AbstractNode *next = 0) const = 0;
+    /** Destructor.
+      * @see release()
+      */
+    //inline ~AbstractNode() { data->release(); };
+
+    /**
+      * @return Returns a reference to the item.
+      */
+    virtual T &getItem() = 0;
+};
+
+template <class T>
+class AbstractList : Container
 {
 private:
-    //! A list node.
-    struct Node
-    {
-        //! Smart node data for COW.
-        struct NodeData
-        {
-            T item; /**< Actual item data. */
-            unsigned int refcount; /**< Number of nodes referencing this data. */
-
-            /** Constructor.
-              * @param item as the item to encapsulate.
-              */
-            inline NodeData(const T &item) : item(item), refcount(1) {};
-            /** Adds a reference for a new node.
-              * @return A pointer to this.
-              * @see release()
-              */
-            inline NodeData *copy() { refcount++; return this; };
-            /** Removes a reference and deletes when appropriate.
-              * @see copy()
-              */
-            inline void release() { if (--refcount == 0) delete this; };
-        };
-
-        NodeData *data; /**< Pointer to the NodeData object containing item data.*/
-        Node *previous; /**< Pointer to previous node in list. */
-        Node *next; /**< Pointer to next node in list. */
-
-        /** Copy constructor.
-          * @param other Node to copy.
-          * @param previous Pointer to previous node.
-          * @param next Pointer to next node.
-          */
-        inline Node(const Node &other, Node *previous = 0, Node *next = 0) : data(other.data->copy()), previous(previous), next(next) {};
-        /** Copy constructor.
-          * @param item Item to store.
-          * @param previous Pointer to previous node.
-          * @param next Pointer to next node.
-          */
-        inline Node(const T &item, Node *previous = 0, Node *next = 0) : data(new NodeData(item)), previous(previous), next(next) {};
-        /** Destructor.
-          * @see release()
-          */
-        inline ~Node() { data->release(); };
-
-        /**
-          * @return Returns a reference to the item.
-          */
-        inline T &getItem();
-    };
+    //virtual Node *getLast() const = 0;
 
     //! Abstract base class for list iterators.
     class BaseIterator
     {
     private:
-        const List *baseList; /**< Pointer to the list this iterator is based on. */
+        const AbstractList *baseList; /**< Pointer to the list this iterator is based on. */
 
     protected:
-        Node *target; /**< Pointer to targeted node.*/
+        AbstractNode<T> *target; /**< Pointer to targeted node.*/
 
     public:
         /** Constructor.
           * @param target Pointer to target node.
           * @param baseList Pointer to base list.
           */
-        inline BaseIterator(Node *target = 0, const List  *baseList = 0) : target(target), baseList(baseList) {};
+        inline BaseIterator(AbstractNode<T> *target = 0, const AbstractList  *baseList = 0) : target(target), baseList(baseList) {};
         /** Copy constructor.
           * @param other Reference of the list to copy.
           */
@@ -97,84 +80,248 @@ private:
         inline BaseIterator &operator+=(int n) { return *this = *this + n; };
         inline BaseIterator &operator-=(int n) { return *this = *this - n; };
 
-        const Node * const getNode() const { return target; };
-        const List * const getBaseList() const { return baseList; };
+        const AbstractNode<T> * const getNode() const { return target; };
+        const AbstractList * const getBaseList() const { return baseList; };
     };
-
-    Node *first; /**< Pointer to first node in list.*/
-    Node *last; /**< Pointer to last node in list.*/
-    unsigned int _size; /**< List size.*/
 
 public:
     //! List iterator.
     class iterator : public BaseIterator
     {
     public:
-        inline iterator(Node *target = 0, const List *baseList = 0) : BaseIterator(target, baseList) {};
+        inline iterator(AbstractNode<T> *target = 0, const AbstractList *baseList = 0) : BaseIterator(target, baseList) {};
         inline iterator(const BaseIterator &other) : BaseIterator(other) {};
 
-        inline T &operator*() { return target->getItem(); };
+        inline T &operator*() { return BaseIterator::target->getItem(); };
     };
 
     //! Const list iterator.
     class const_iterator : public BaseIterator
     {
     public:
-        inline const_iterator(Node *target = 0, const List *baseList = 0) : BaseIterator(target, baseList) {};
+        inline const_iterator(AbstractNode<T> *target = 0, const AbstractList *baseList = 0) : BaseIterator(target, baseList) {};
         inline const_iterator(const BaseIterator &other) : BaseIterator(other) {};
 
-        inline const T &operator*() { return target->getItem(); };
+        inline const T &operator*() { return BaseIterator::target->getItem(); };
 
     };
 
-    inline List() : first(0), last(0), _size(0) {};
-    List(const List<T> &other);
+    inline AbstractList() {};
+    //AbstractList(const AbstractList<T> &other);
 
     /** Append one item to list.
       * @param item The item to append.
       */
-    void append(const T &item);
+    virtual void append(const T &item) = 0;
     /** Prepend one item to list.
       * @param item The item to prepend.
       */
-    void prepend(const T &item);
+    virtual void prepend(const T &item) = 0;
     /** Insert one item into list.
       * @param item The item to insert.
       * @param pos The position for the new item.
       */
-    void insert(const T &item, unsigned int pos);
+    virtual void insert(const T &item, unsigned int pos) = 0;
     /** Remove one item from list.
       * @param pos Position of the item to remove.
       */
-    void remove(unsigned int pos);
+    virtual void remove(unsigned int pos) = 0;
     /** Delete all list items. */
-    void clear();
+    virtual void clear() = 0;
 
     /** @return Returns an iterator to the first item of the list.  */
-    inline iterator begin() { return iterator(first, this); };
+    virtual iterator begin() = 0;
     /** @return Returns a const_iterator to the first item of the list.  */
-    inline const_iterator begin() const { return const_iterator(first, this); };
+    virtual const_iterator begin() const = 0;
     /** @return Returns an iterator pointing after the last item in the list.  */
-    inline iterator end() { return iterator(0, this); };
+    virtual iterator end() = 0;
     /** @return Returns a const_iterator pointing after the last item in the list.  */
-    inline const_iterator end() const { return const_iterator(0, this); };
+    virtual const_iterator end() const = 0;
     /** Access one element based on its position.
       * @param pos The position to access.
       * @return A reference to the item.
       */
-    const T &at(unsigned int pos) const;
+    virtual const T &at(unsigned int pos) const = 0;
     /** @return Returns true if the list is empty. */
-    inline bool isEmpty() const { return size == 0; };
+    virtual bool isEmpty() const = 0;
     /** @return The list's size.*/
-    inline unsigned int size() const { return _size; };
+    virtual unsigned int size() const = 0;
+
+    /*
+    bool operator==(const AbstractList &other) const;
+    bool operator!=(const AbstractList &other) const;
+    AbstractList operator+(const T &item) const;
+    AbstractList operator+(const AbstractList &other) const;
+    AbstractList &operator+=(const T &item);
+    AbstractList &operator+=(const AbstractList &other);
+    T &operator[](unsigned int pos) const= 0;
+    */
+};
+
+template <class T>
+struct NaiveNode : public AbstractNode<T>
+{
+    T item;
+
+    inline NaiveNode(const T &item, AbstractNode *previous = 0, AbstractNode *next = 0) : item(item), AbstractNode(previous, next) {};
+    inline NaiveNode(const NaiveNode &other, AbstractNode *previous = 0, AbstractNode *next = 0) : item(other.item), AbstractNode(previous, next) {};
+    virtual inline NaiveNode *copy(AbstractNode *previous = 0, AbstractNode *next = 0) const { return new NaiveNode(*this, previous, next); };
+
+    virtual inline T &getItem() { return item; };
+};
+
+template <class T, class _Node = NaiveNode<T> >
+class List : public AbstractList<T>
+{
+private:
+    AbstractNode<T> *first; /**< Pointer to first node in list.*/
+    AbstractNode<T> *last; /**< Pointer to last node in list.*/
+    unsigned int _size; /**< List size.*/
+
+    //virtual AbstractNode *getLast() const { return last; };
+
+public:
+    inline List() : first(0), last(0), _size(0) {};
+    List(const List &other);
+
+    virtual void append(const T &item);
+    virtual void prepend(const T &item);
+    virtual void insert(const T &item, unsigned int pos);
+    virtual void remove(unsigned int pos);
+    virtual void clear();
+
+    virtual inline iterator begin() { return iterator(first, this); };
+    virtual inline const_iterator begin() const { return const_iterator(first, this); };
+    virtual inline iterator end() { return iterator(0, this); };
+    virtual inline const_iterator end() const { return const_iterator(0, this); };
+    virtual const T &at(unsigned int pos) const;
+    virtual inline bool isEmpty() const { return size() == 0; };
+    virtual inline unsigned int size() const { return _size; };
 
     bool operator==(const List &other) const;
-    inline bool operator!=(const List &other) const { return (! *this == other); };
+    inline bool operator!=(const List &other) const { return ! (*this == other); };
     inline List operator+(const T &item) const { List result(*this); result.append(item); return result; };
     inline List operator+(const List &other) const { List result(*this); result += other; return result; };
-    inline List &operator+=(const T &item) { append(item); return *this; }
-    inline List &operator+=(const List &other);
-    inline T &operator[](unsigned int pos) const;
+    inline List &operator+=(const T &item) { append(item); };
+    List &operator+=(const List &other);
+    T &operator[](unsigned int pos) { iterator it(this->begin()); while (pos--) ++it; return *it; };
+};
+
+template <class T>
+class AbstractListData //: public AbstractList<T>
+{
+protected:
+    unsigned int refcount;
+
+public:
+    inline AbstractListData() : refcount(1) {};
+
+    virtual inline AbstractListData *copy() { refcount++; return this; };
+    void release();
+
+    virtual AbstractListData *detach() = 0;
+};
+
+template <class T>
+class SmartListData : public List<T, NaiveNode<T> >, public AbstractListData<T>
+{
+public:
+    virtual inline SmartListData *copy() { refcount++; return this; };
+    virtual SmartListData *detach();
+};
+
+template <class T, class _ListData>
+class AbstractSmartList : public AbstractList<T>
+{
+private:
+    _ListData *listData;
+
+    inline void detach() { listData = listData->detach(); };
+    //virtual AbstractNode *getLast() const { return listData->getLast(); };
+
+public:
+    inline AbstractSmartList() : listData(new _ListData) {};
+    inline AbstractSmartList(const AbstractSmartList &other) : listData(other.listData->copy()) {};
+
+    virtual inline void append(const T &item) { detach(); listData->append(item); };
+    virtual inline void prepend(const T &item){ detach(); listData->prepend(item); };
+    virtual inline void insert(const T &item, unsigned int pos){ detach(); listData->insert(item, pos); };
+    virtual inline void remove(unsigned int pos){ detach(); listData->remove(pos); };
+    virtual inline void clear() { detach(); listData->clear(); };
+
+    virtual inline iterator begin() { return listData->begin(); };
+    virtual inline const_iterator begin() const { return listData->begin(); };
+    virtual inline iterator end() { return listData->end(); };
+    virtual inline const_iterator end() const { return listData->end(); };
+    virtual inline const T &at(unsigned int pos) const { return listData->at(pos); };
+    virtual inline bool isEmpty() const { return listData->isEmpty(); };
+    virtual inline unsigned int size() const { return listData->size(); };
+
+    inline bool operator==(const AbstractSmartList &other) const { return *listData == other.listData; };
+    inline bool operator!=(const AbstractSmartList &other) const { return ! (*this == other); };
+    inline AbstractSmartList operator+(const T &item) const { AbstractSmartList result(*this); result.append(item); return result; };
+    inline AbstractSmartList operator+(const AbstractSmartList &other) const { AbstractSmartList result(*this); result += other; return result; };
+    inline AbstractSmartList &operator+=(const T &item) { append(item); };
+    inline AbstractSmartList &operator+=(const AbstractSmartList &other) { detach(); *listData += *(other.listData); return *this; };
+    inline T &operator[](unsigned int pos) { iterator it(this->begin()); while (pos--) ++it; return *it; };
+};
+
+template <class T>
+class SmartList : public AbstractSmartList<T, SmartListData<T> >
+{
+
+};
+
+template <class T>
+struct SmartNode : AbstractNode<T>
+{
+    //! Smart node data for COW.
+    class NodeData
+    {
+    private:
+        T item; /**< Actual item data. */
+        unsigned int refcount; /**< Number of nodes referencing this data. */
+
+    public:
+        /** Constructor.
+          * @param item as the item to encapsulate.
+          */
+        inline NodeData(const T &item) : item(item), refcount(1) {};
+        /** Adds a reference for a new node.
+          * @return A pointer to this.
+          * @see release()
+          */
+        inline NodeData *copy() { refcount++; return this; };
+        /** Removes a reference and deletes when appropriate.
+          * @see copy()
+          */
+        inline void release() { if (--refcount == 0) delete this; };
+        inline NodeData *detach();
+
+        inline T &getItem() { return item; };
+    };
+
+    NodeData *data; /**< Pointer to the NodeData object containing item data.*/
+
+    inline SmartNode(const SmartNode &other, AbstractNode *previous = 0, AbstractNode *next = 0) : data(other.data->copy()), AbstractNode(previous, next) {};
+    inline SmartNode(const T &item, AbstractNode *previous = 0, AbstractNode *next = 0) : data(new NodeData(item)), AbstractNode(previous, next) {};
+    virtual inline SmartNode *copy(AbstractNode *previous = 0, AbstractNode *next = 0) const { return new SmartNode(*this, previous, next); };
+
+    virtual inline T &getItem() { data = data->detach(); return data->getItem(); };
+};
+
+template <class T>
+class RepetitiveListData : public AbstractListData<T>, public List<T, SmartNode<T> >
+{
+public:
+    virtual inline RepetitiveListData *copy() { refcount++; return this; };
+    virtual RepetitiveListData *detach();
+};
+
+template <class T>
+class RepetitiveList : public AbstractSmartList<T, RepetitiveListData<T> >
+{
+
 };
 
 #include "List.cpp"
