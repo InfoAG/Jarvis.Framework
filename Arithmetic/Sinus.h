1a4d12575ed6e6013c464a397a324badc5113504
Alexander Schlueter <arleckshunt@googlemail.com>
2012-06-12 18:49:38 +0200

    fixed copy() for Sinus

--- Arithmetic/Sinus.h
+++ Arithmetic/Sinus.h
@@ -8,7 +8,7 @@ namespace CAS {
 class Sinus : public AbstractUnaryOperation
 {
 public:
-    virtual inline AbstractArithmetic *copy() const { return new Addition(*this); };
+    virtual inline AbstractArithmetic *copy() const { return new Sinus(*this); };
 
     virtual inline AbstractArithmetic *eval(const EvalInfo &ei) const { return this->copy(); };
 };
