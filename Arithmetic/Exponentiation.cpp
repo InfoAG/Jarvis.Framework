9fc62d5a5b161af0a0cdce370d1e77d27b0f458e
Alexander Schlueter <arleckshunt@googlemail.com>
2012-09-03 18:51:23 +0200

    fix exponentiation simplificatiion

--- Arithmetic/Exponentiation.cpp
+++ Arithmetic/Exponentiation.cpp
@@ -8,7 +8,9 @@ std::unique_ptr<CAS::AbstractArithmetic> Exponentiation::eval(const EvalInfo &ei
     if (firstOpResult->type() == NUMBERARITH && secondOpResult->type() == NUMBERARITH)
         return make_unique<NumberArith>(pow(*(static_cast<NumberArith*>(firstOpResult.get())), *(static_cast<NumberArith*>(secondOpResult.get()))));
     else if (firstOpResult->type() == EXPONENTIATION)
-        return make_unique<Exponentiation>(std::move(static_cast<Exponentiation*>(firstOpResult.get())->getFirstOp()), make_unique<Multiplication>(std::move(static_cast<Exponentiation*>(firstOpResult.get())->getSecondOp()), std::move(secondOpResult)));
+        return Exponentiation(std::move(static_cast<Exponentiation*>(firstOpResult.get())->getFirstOp()),
+                              make_unique<Multiplication>(std::move(static_cast<Exponentiation*>(firstOpResult.get())->getSecondOp()),
+                                                          std::move(secondOpResult))).eval(ei);
     else if (firstOpResult->type() == NUMBERARITH && static_cast<NumberArith*>(firstOpResult.get())->getValue() == 1)
         return firstOpResult;
     else if (secondOpResult->type() == NUMBERARITH) {
