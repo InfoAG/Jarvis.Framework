36ab3228e4de7c07bca97c20238858e8ab2a1a22
Alexander Schlueter <arleckshunt@googlemail.com>
2012-09-16 21:01:16 +0200

    fix modulo's toString()

--- Arithmetic/Modulo.cpp
+++ Arithmetic/Modulo.cpp
@@ -10,6 +10,18 @@ std::unique_ptr<AbstractArithmetic> Modulo::eval(const EvalInfo &ei) const
     else return make_unique<Modulo>(std::move(first_op_result), std::move(second_op_result));
 }
 
+std::string Modulo::toString() const
+{
+    std::string result;
+    if (first_op->type() == ADDITION || first_op->type() == SUBTRACTION || first_op->type() == MULTIPLICATION || first_op->type() == DIVISION || first_op->type() == ASSIGNMENT)
+        result = "(" + first_op->toString() + ")";
+    else result = first_op->toString();
+    result += "%";
+    if (second_op->type() == ADDITION || second_op->type() == SUBTRACTION || second_op->type() == MULTIPLICATION || second_op->type() == DIVISION || second_op->type() == ASSIGNMENT)
+        return result + "(" + second_op->toString() + ")";
+    else return result + second_op->toString();
+}
+
 bool Modulo::equals(const AbstractArithmetic *other) const
 {
     return other->type() == MODULO && first_op->equals(static_cast<const Modulo*>(other)->getFirstOp().get()) && second_op->equals(static_cast<const Modulo*>(other)->getSecondOp().get());
