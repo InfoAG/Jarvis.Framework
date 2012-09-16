734950cee6eb7192b230b1b5f7ea2de2bb6f6c63
Alexander Schlueter <arleckshunt@googlemail.com>
2012-09-16 20:53:32 +0200

    fix modulo when operands are not numbers

--- Arithmetic/Modulo.cpp
+++ Arithmetic/Modulo.cpp
@@ -7,7 +7,7 @@ std::unique_ptr<AbstractArithmetic> Modulo::eval(const EvalInfo &ei) const
     auto first_op_result = first_op->eval(ei), second_op_result = second_op->eval(ei);
     if (first_op_result->type() == NUMBERARITH && second_op_result->type() == NUMBERARITH)
         return make_unique<NumberArith>(*(static_cast<NumberArith*>(first_op_result.get())) % *(static_cast<NumberArith*>(second_op_result.get())));
-    else return Modulo(std::move(first_op_result), std::move(second_op_result)).eval(ei);
+    else return make_unique<Modulo>(std::move(first_op_result), std::move(second_op_result));
 }
 
 bool Modulo::equals(const AbstractArithmetic *other) const
